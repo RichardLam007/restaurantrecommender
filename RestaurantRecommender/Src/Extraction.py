4ff71b071804e77ec14d781bcc6f44e63035a204
rlam <richardlam007@gmail.com>
2013-04-24 00:41:16 -0400

    extraction first commit

---
+++ RestaurantRecommender/Src/Extraction.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 24, 2013
+
+@author: EliteCrew
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
