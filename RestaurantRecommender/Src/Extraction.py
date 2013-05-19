356c0acc814837d208f2208d0ced135292e363b1
Kathy <kyashi249@yahoo.com>
2013-05-19 17:09:22 -0400

    added a function to fetch the next review in the dataset (or indicate that there is no more) and included storage of the dictionaries into fi

--- RestaurantRecommender/Src/Extraction.py
+++ RestaurantRecommender/Src/Extraction.py
@@ -3,14 +3,17 @@ Created on Apr 24, 2013
 
 @author: Kathy
 '''
-import sys, traceback, ast
+import sys, traceback, ast, json
 
 class Extractor:
+    #Constructor
     def __init__(self):
         self.users = dict()
         #self.reviews = dict()
         self.businesses = dict()
+        self.fileoffset = 0
     
+    #Extract the necessary information on the users and restaurants
     def extractInfo(self):
         f = open('tmpNFvucr', 'r')
     
@@ -20,14 +23,14 @@ class Extractor:
             #some of the entries result in an issue with malformed strings for some reason
             except ValueError:
                 continue
-    
+            
             if line['type'] == "user":
                 #add the user to the list if they are not already in it
                 if line['user_id'] not in self.users:
                     self.users.update({line['user_id']: dict()})
-                    self.users[line['user_id']].update({'name': line['name'], 'reviews': [], 'businesses': []})                
+                    self.users[line['user_id']].update({'name': line['name'], 'reviews': [], 'businesses': []})      
             #if the entry is about a user's review
-            elif line['type'] == "review":
+            if line['type'] == "review":
                 #add each restaurant the user has visited to that user's list    
                 if line['business_id'] not in self.users[line['user_id']]['businesses']:
                     self.users[line['user_id']]['businesses'].append('business_id')
@@ -42,5 +45,41 @@ class Extractor:
                 if line['business_id'] not in self.businesses and line['open'] == "true":
                     self.businesses.update({line['business_id']: dict()})
                     self.businesses[line['business_id']].update({'name': line['name'], 'state': line['state'], 'city': line['city'], 'categories': ast.literal_eval(line['categories']) })       
-        f.close()     
-  
+        f.close()
+
+        #Store the dictionaries as json objects
+        with open('userinfo.json', 'w') as outfile:
+            json.dump(self.users, outfile)
+        with open('bussinfo.json', 'w') as outfile:
+            json.dump(self.businesses, outfile)     
+
+    #Obtain the next review entry from the dataset
+    def nextReview(self):
+        f = open('tmpNFvucr', 'r')
+        
+        #skip to the middle of the file according to the offset
+        f.seek(self.fileoffset)
+        tline = f.readline().strip()
+        while tline == "":
+            tline = f.readline().strip() 
+        
+        try:
+            currLine = ast.literal_eval(tline)
+        except:
+            self.fileoffset = 0  #reset the offset into the file back to the beginning
+            return {}  #indicate there are no more reviews in the dataset
+        
+        #ignore all the entries about just the users
+        while currLine['type'] == "user":
+            currLine = ast.literal_eval(f.readline())
+        
+        #return an empty dictionary if there are no more reviews in the dataset
+        if currLine['type'] == "business":
+            currLine = {}
+            self.fileoffset = 0  #reset the offset into the file back to the beginning
+        #otherwise adjust the offset into the file for the next read  
+        else:
+            self.fileoffset = f.tell() - 1
+        
+        f.close()
+        return currLine
