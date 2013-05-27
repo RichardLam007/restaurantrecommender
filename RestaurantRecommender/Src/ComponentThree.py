32b2bb45c50a19cca1408ea120a4204a68c581c5
rlam <richardlam007@gmail.com>
2013-05-27 01:22:06 -0400

    added negation to component three

--- RestaurantRecommender/Src/ComponentThree.py
+++ RestaurantRecommender/Src/ComponentThree.py
@@ -21,6 +21,8 @@ class ComponentThree:
         '''
         return the computed addition of the two given attributes/reasons
         '''
+        if base < 0:
+            return additive
         return base + additive/(sqrt(abs(base)))
         
     def processReview(self, reviewText):
@@ -33,41 +35,55 @@ class ComponentThree:
         adjectives = dict()
         reasons = dict()
         
+        previousPreviousWord = None
+        previousWord = None
+        
         for row in taggedWords:
             if self.debug == True:
                 print row
-            word = row[0]
+            word = row[0].lower()
             POS = row[1]
+            negator = 1
+            
+            if previousPreviousWord in ['no', 'not', 'bad', 'terrible', 'horrible', 'worst']:
+                negator = -1
+            
             if POS in ["JJ", "JJR"]: #if verbs
                 adjective = self.vocabularyObject.getMatchingAdjective(word) #check for adjective via synonym
                 if not adjective == None:
                     if word in adjectives:
-                        adjectives[word] = self.additionFormula(adjectives[word], 1)
+                        adjectives[word] = self.additionFormula(adjectives[word], 1*negator)
                     else:
-                        adjectives[word] = 1
+                        adjectives[word] = 1*negator
                 else:
                     adjective = self.vocabularyObject.getMatchingAntonymAdjective(word) #check for adjective via antonym
                     if not adjective == None:
                         if word in adjectives:
-                            adjectives[word] = self.additionFormula(adjectives[word], -1)
+                            adjectives[word] = self.additionFormula(adjectives[word], -1*negator)
                         else:
-                            adjectives[word] = -1
+                            adjectives[word] = -1*negator
             if POS in ["NN", "NNS", "FW"]: #if noun, foreign word
                 if self.vocabularyObject.isReasonForVisit(word):
                     if word in reasons:
-                        reasons[word] = self.additionFormula(reasons[word], 1)
+                        reasons[word] = self.additionFormula(reasons[word], 1*negator)
                     else:
-                        reasons[word] = 1
+                        reasons[word] = 1*negator
+                        
+            previousWord = word
+            previousPreviousWord = previousWord
         ################
         # NOT DONE YET
         ################
         return adjectives, reasons
         
-    def processReviews(self):
+    def processReviews(self, maxNumber):
         '''
         Loops through all available reviews from extraction object, processes them, and pushes results into the appropriate restaurant objects
         '''
-        while True:
+        reviewCount = 0
+        while reviewCount < maxNumber:
+            reviewCount += 1
+            print "processing review count: " + str(reviewCount)
             review = self.extraction.nextReview()
             if len(review) == 0:
                 return # no more
